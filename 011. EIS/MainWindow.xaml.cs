23893c460850ae1ba76794fcc9de456327b6a1a6
thgPwls98 <127263928+thgPwls98@users.noreply.github.com>
2023-05-26 15:52:11 +0900

    Add files via upload

--- 011. EIS/MainWindow.xaml.cs
+++ 011. EIS/MainWindow.xaml.cs
@@ -1,18 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using MySql.Data.MySqlClient;
 
 namespace _011.EIS
 {
@@ -35,6 +25,7 @@ namespace _011.EIS
             InitializeComponent();
 
             conn = new MySqlConnection(connStr);
+            DisplayDataGrid();
         }
 
         private void btnInsert_Click(object sender, RoutedEventArgs e)
@@ -63,17 +54,152 @@ namespace _011.EIS
             dept=cbDept.Text;
             pos= cbPos.Text;
 
-            conn.Open();
 
-            string spl=string.Format("INSERT INTO eis_table(name, department, position, gender, date_enter, date_exit, contact, comment)"
+            conn.Open();
+            try
+            {
+                string spl = string.Format("INSERT INTO eis_table(name, department, position, gender, date_enter, date_exit, contact, comment)"
                 + "VALUES('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}')",
                 txtName.Text, dept, pos, gender, dateEnter, dateExit, txtContact.Text, txtComment.Text);
 
-            MySqlCommand cmd = new MySqlCommand(spl, conn);
-            if (cmd.ExecuteNonQuery() == 1)
-                MessageBox.Show("성공적으로 추가되었습니다.");
+                MySqlCommand cmd = new MySqlCommand(spl, conn);
+                if (cmd.ExecuteNonQuery() == 1)
+                    MessageBox.Show("성공적으로 추가되었습니다.");
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
 
             conn.Close();
+            InitControls();
+            DisplayDataGrid();
+        }
+
+        private void InitControls()
+        {
+            txtEid.Text = "";
+            txtName.Text = "";
+            txtContact.Text = "";
+            txtComment.Text = "";
+            cbDept.SelectedIndex = -1;
+            cbPos.SelectedIndex = -1;
+            rbFemale.IsChecked = false;
+            rbMale.IsChecked = false;
+            dpEnter.Text = "";
+            dpExit.Text = "";
+        }
+
+        private void btnLoadData_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayDataGrid();
+        }
+
+        private void DisplayDataGrid()
+        {
+            conn.Open();
+
+            string sql = "SELECT * FROM eis_table";
+
+
+            try
+            {
+                MySqlDataAdapter da = new MySqlDataAdapter(sql, conn);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dataGrid.ItemsSource = ds.Tables[0].DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            conn.Close();
+        }
+
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)  //sender=datagrid
+        {
+            DataGrid dg=sender as DataGrid; //(DataGrid)sender;
+            DataRowView rowView = dg.SelectedItem as DataRowView;
+
+            if (rowView == null)
+                return;
+
+            txtEid.Text = rowView.Row[0].ToString();
+            txtName.Text = rowView.Row[1].ToString();
+            cbDept.Text = rowView.Row[2].ToString();
+            cbPos.Text = rowView.Row[3].ToString();
+
+            if (rowView.Row[4].ToString() == "남성")
+            {
+                rbMale.IsChecked = true;
+                rbFemale.IsChecked = false;
+            }
+            else
+            {
+                rbMale.IsChecked = false;
+                rbFemale.IsChecked = true;
+            }
+
+            dpEnter.Text = rowView.Row[5].ToString();
+            dpEnter.Text = rowView.Row[6].ToString();
+            txtContact.Text = rowView.Row[7].ToString();
+            txtComment.Text= rowView.Row[8].ToString();
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            if (rbMale.IsChecked == true)
+            {
+                gender = "남성";
+            }
+            else
+            {
+                gender = "여성";
+            }
+
+            dateEnter=dpEnter.Text;
+            dateExit=dpExit.Text;
+
+            dept=cbDept.Text;
+            pos=cbPos.Text;
+
+            conn.Open();
+            try
+            {
+                string sql = string.Format(
+                    "UPDATE eis_table SET name='{0}', department='{1}', position='{2}', gender='{3}', date_enter='{4}', date_exit='{5}',contact='{6}',comment='{7}'" +
+                    "WHERE eid={8}", txtName.Text, dept, pos, gender, dateEnter, dateExit, txtContact.Text, txtComment.Text, txtEid.Text); //eid는 int이기 떄문에 ''가 필요없음
+                MessageBox.Show(sql);
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                if (cmd.ExecuteNonQuery() == 1)
+                    MessageBox.Show("성공적으로 수정되었습니다.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            conn.Close();
+            InitControls();
+            DisplayDataGrid();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            conn.Open();
+            try
+            {
+                string sql = string.Format("DELETE FROM eis_table WHERE eid={0}", txtEid.Text);
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                if (cmd.ExecuteNonQuery() == 1)
+                    MessageBox.Show("삭제가 성공되었습니다.");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            conn.Close();
+            InitControls();
+            DisplayDataGrid();
         }
     }
 }
